ca5152ea76821a6dfaa682e5f52f21b177fa89b3
fangyong <690615290@qq.com>
2014-07-14 22:25:58 +0800

    update the tab

--- src/com/baiduvolunteer/activity/HomeAct.java
+++ src/com/baiduvolunteer/activity/HomeAct.java
@@ -6,8 +6,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TabWidget;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baidu.api.AccessTokenManager;
@@ -76,30 +79,38 @@ public class HomeAct extends Activity {
 		tabHost = (TabHost) findViewById(android.R.id.tabhost);
 		tabHost.setup();
 
-		tabHost.addTab(tabHost
-				.newTabSpec("tab_index")
-				.setIndicator("首頁",
-						getResources().getDrawable(R.drawable.ic_launcher))
-				.setContent(R.id.tab_index));
-		tabHost.addTab(tabHost
-				.newTabSpec("tab_activities")
-				.setIndicator("活动",
-						getResources().getDrawable(R.drawable.ic_launcher))
-				.setContent(R.id.tab_activities));
-		tabHost.addTab(tabHost
-				.newTabSpec("tab_usercenter")
-				.setIndicator("我的",
-						getResources().getDrawable(R.drawable.ic_launcher))
-				.setContent(R.id.tab_usercenter));
-		tabHost.addTab(tabHost
-				.newTabSpec("tab_more")
-				.setIndicator("更多",
-						getResources().getDrawable(R.drawable.ic_launcher))
-				.setContent(R.id.tab_more));
+		initTabs();
+	}
 
-		// View v = tabWidget.getChildTabViewAt(1);
-		//
-		// v.setBackgroundColor(0xffffffff);
+	void initTabs() {
+		View lb1 = getLayoutInflater().inflate(R.layout.label_tabhost, null);
+		TextView tx1 = (TextView) lb1.findViewById(R.id.text);
+		tx1.setText("首页");
+		ImageView iv1 = (ImageView) lb1.findViewById(R.id.img);
+		iv1.setImageResource(R.drawable.tab1);
+		View lb2 = getLayoutInflater().inflate(R.layout.label_tabhost, null);
+		TextView tx2 = (TextView) lb2.findViewById(R.id.text);
+		tx2.setText("活动");
+		ImageView iv2 = (ImageView) lb2.findViewById(R.id.img);
+		iv2.setImageResource(R.drawable.tab2);
+		View lb3 = getLayoutInflater().inflate(R.layout.label_tabhost, null);
+		TextView tx3 = (TextView) lb3.findViewById(R.id.text);
+		tx3.setText("我的");
+		ImageView iv3 = (ImageView) lb3.findViewById(R.id.img);
+		iv3.setImageResource(R.drawable.tab3);
+		View lb4 = getLayoutInflater().inflate(R.layout.label_tabhost, null);
+		TextView tx4 = (TextView) lb4.findViewById(R.id.text);
+		tx4.setText("更多");
+		ImageView iv4 = (ImageView) lb4.findViewById(R.id.img);
+		iv4.setImageResource(R.drawable.tab4);
+		tabHost.addTab(tabHost.newTabSpec("tab_index").setIndicator(lb1)
+				.setContent(R.id.tab_index));
+		tabHost.addTab(tabHost.newTabSpec("tab_activities").setIndicator(lb2)
+				.setContent(R.id.tab_activities));
+		tabHost.addTab(tabHost.newTabSpec("tab_usercenter").setIndicator(lb3)
+				.setContent(R.id.tab_usercenter));
+		tabHost.addTab(tabHost.newTabSpec("tab_more").setIndicator(lb4)
+				.setContent(R.id.tab_more));
 	}
 
 }
