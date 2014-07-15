e65336e81d2364808b6e91bead2b23de4884240c
fangyong <fangyong@hsltech.com>
2014-07-15 10:40:49 +0800

    add the loginrequest

---
+++ src/com/baiduvolunteer/http/LoginRequest.java
@@ -0,0 +1,32 @@
+package com.baiduvolunteer.http;
+
+import java.util.HashMap;
+
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
+
+public class LoginRequest extends BaseRequest {
+
+	private String uid;
+
+	public LoginRequest(String uid) {
+		this.uid = uid;
+	}
+
+	@Override
+	protected String url() {
+		return "";
+	}
+
+	@Override
+	protected void generateParams(HashMap<String, String> map) {
+		map.put("uid",uid);
+		map.put("signature",SignatureTool.getSignature(map));
+		
+	}
+
+	@Override
+	protected HttpMethod getMethod() {
+		return HttpMethod.POST;
+	}
+
+}
